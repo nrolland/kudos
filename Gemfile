ccd14c6949400df4b7ae1326ed7e01522c5f1a5d
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2011-11-04 22:51:30 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://rubygems.org'
+
+gem 'bcrypt-ruby'
+gem 'jquery-rails'
+gem 'less-rails-bootstrap'
+gem 'rails', '3.1.1'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'sass-rails', '~> 3.1.4'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'growl_notify'
+  gem 'guard-rspec'
+  gem 'turn', '< 0.8.3'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kudos::Application.config.secret_token = '4c9fcd33d540b9e5ed4b383fecb5a856780f9788c4ec379a4b4d55981f4ff0e8cd73a84fbeaa02c92940a26eba0799b200a1e1dbc607e5406bd59e1ff30a4573'

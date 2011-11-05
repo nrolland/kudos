84400e3bcae58c225c63c18af9a437de4f5d7634
Antonio <antonio@santosvelasco.com>
2011-11-05 14:00:25 +0100

    More season model spec tests

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,7 +1,18 @@
 class Season < ActiveRecord::Base
-  validates :start_date, :end_date, :starting_kudos, :max_kudos_per_user, :presence => true
+  validates :start_date, :end_date, :starting_kudos, :max_kudos_per_user,
+    :presence => true
+  validates :starting_kudos, :max_kudos_per_user,
+    :numericality => { :only_integer => true, :greater_than => 0 }
+
+  validate :season_cannot_start_before_it_ends
 
   belongs_to :group
   has_many :kudos
   has_many :prizes
+
+  def season_cannot_start_before_it_ends
+    if not start_date.nil? and not end_date.nil?
+      errors.add(:end_date, "The season cannot end before it starts") if end_date < start_date
+    end
+  end
 end
--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -29,5 +29,20 @@ describe Season do
       @season.max_kudos_per_user = nil
       @season.should_not be_valid
     end
+
+    it "should only accept positive integer starting kudos" do
+      @season.starting_kudos = -2
+      @season.should_not be_valid
+    end
+
+    it "should only accept positive integer maximum kudos per user" do
+      @season.max_kudos_per_user = -2
+      @season.should_not be_valid
+    end
+
+    it "should not end before it starts" do
+      @season.end_date = @season.start_date - 1.year
+      @season.should_not be_valid
+    end
   end
 end

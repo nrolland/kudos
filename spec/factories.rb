ff14c811cdf8db0d92092c5d86b7e12550c15b11
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2011-11-05 17:01:17 +0100

    Add a bunch of factories: kudo, user...

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,40 @@
-Factory.define :season do |season|
-  season.start_date Time.now
-  season.end_date Time.now + 1.year
-  season.starting_kudos 2
-  season.max_kudos_per_user 10
+Factory.define :season do |s|
+  s.start_date Time.now
+  s.end_date Time.now + 1.year
+  s.starting_kudos 2
+  s.max_kudos_per_user 10
+
+  s.association :group, :factory => :group
+end
+
+Factory.define :group do |g|
+  g.name "Test group"
+  g.description "Just a factoried test group"
+end
+
+Factory.define :user do |u|
+  u.name "Chuck Norris"
+  u.email "chuck@example.net"
+  u.password "pass"
+  u.password_confirmation "pass"
+  u.role :user
+
+  u.association :group, :factory => :group
+end
+
+
+Factory.define :kudo do |k|
+  k.message "Oooouuhhh yyyyyeah"
+
+  k.association :from, :factory => :from
+  k.association :to, :factory => :to
+  k.association :season, :factory => :season
+end
+
+Factory.define :comment do |c|
+  c.text "Oooouuhhh yyyyyeah"
+  c.value 0
+
+  c.association :user, :factory => :user
+  c.association :kudo, :factory => :kudo
 end

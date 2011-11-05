d340330d600782f70a96b0ce5c4ada994f73d172
felix <felix.g.borrego@gmail.com>
2011-11-05 10:21:08 +0100

    Add validation and relationship on group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,8 @@
 class Group < ActiveRecord::Base
+  has_many :seasons
+  has_many :users
+  
+  validates :name,        :presence => true,
+                          :length    => {:maximum => 50, :minimum => 3 }
+  validates :description,  :presence => true
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @attr = { :name => "Viavansi Group", :description => "example of description" }
+  end
+  
+  it "should required a name" do
+    no_name_group =Group.new(@attr.merge(:name=>""))
+    no_name_group.should_not be_valid
+  end  
+  
+  it "should reject name that are too long" do
+    long_name="Name too big"*50
+    long_name_group =Group.new(@attr.merge(:name=>long_name))
+    long_name_group.should_not be_valid
+  end  
+  
+
+  it "should reject name that are too small" do
+    sort_name="aa"
+    sort_name_group=Group.new(@attr.merge(:name=>sort_name))
+    sort_name_group.should_not be_valid
+  end   
+  
+  it "should required a description" do
+    no_description_group =Group.new(@attr.merge(:description=>""))
+    no_description_group.should_not be_valid
+  end  
+  
 end

e558ae839c53fac72dc50379f06bcbf549c5015c
Raúl Recacha <rrecacha@viavansi.com>
2011-11-05 10:27:27 +0100

    Add basic test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not save user without all required fields filled" do 
+    user = User.new
+    user.should_not be_valid
+  end
+  
+  pending "email format"
+  
+  pending "email uniqueness"
+  
+  pending "role valid"
+  
 end

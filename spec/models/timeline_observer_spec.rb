cc8a056c8041ebc374583ce6f34267170ca69a76
Antonio <antonio@santosvelasco.com>
2011-11-05 19:02:08 +0100

    Add TimelineObserver spec

--- spec/models/timeline_observer_spec.rb
+++ spec/models/timeline_observer_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe TimelineObserver do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a new entry in the timeline if a kudo is created" do
+    original_timeline_size = Timeline.all.size
+    kudos = Factory(:kudos)
+    updated_timeline_size = Timeline.all.size
+    updated_timeline_size.should be > original_timeline_size
+  end
 end
